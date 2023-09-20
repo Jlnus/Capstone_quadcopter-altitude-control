39e57ea81f64fcc599318239f57b4f0e869a4c55
Jinus <69241185+Jlnus@users.noreply.github.com>
2023-09-20 11:37:04 +0900

    Update motor_calibration.ino

--- actuator/motor_calibration/motor_calibration.ino
+++ actuator/motor_calibration/motor_calibration.ino
@@ -1,7 +1,12 @@
+/*
+      Servo 라이브러리를 이용한 ESC calibration 
+      ESC에 최소 신호와 최고 신호 등록
+*/
 #include <Servo.h>
 
 #define MAX_SIGNAL 2000
-#define MIN_SIGNAL 700
+#define MIN_SIGNAL 800
+
 int motorA_pin = 3;
 int motorB_pin = 9;
 int motorC_pin = 10;
@@ -15,7 +20,8 @@ void setup() {
     Serial.begin(9600); // open the serial port
     Serial.println("Program begin...");
     Serial.println("This program will calibrate the ESC.");
-
+    
+    // 핀 할당
     motorA.attach(motorA_pin);
     motorB.attach(motorB_pin);
     motorC.attach(motorC_pin);
@@ -29,8 +35,8 @@ void setup() {
     motorD.writeMicroseconds(MAX_SIGNAL);
 
     // Wait for input
-    while (!Serial.available())
-        ; // using while "wait" for input, using if at loop stop "when" serial input
+    while (!Serial.available()); 
+    // using while "wait" for input, using if at loop stop "when" serial input
     Serial.read();
     delay(3000);
     // Send min output
