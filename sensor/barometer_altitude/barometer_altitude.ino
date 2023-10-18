9c00c7962c44d53d13a27dc1bfc21100ca57bbf5
Jlnus <zmdk0902@naver.com>
2023-10-18 11:18:26 +0900

    Update barometer / add sea pressure url

--- sensor/barometer_altitude/barometer_altitude.ino
+++ sensor/barometer_altitude/barometer_altitude.ino
@@ -5,35 +5,36 @@
 #include <Wire.h>
 
 float altitude, pressure, temperature;
-const float sea_pressure = 1009.9;  // 해면기압
+const float sea_pressure = 1009.9; // 해면기압
+// https://www.kma.go.kr/weather/observation/aws_table_popup.jsp
 
-MS5611 MS5611(0x77);  // MS5611 class 생성
+MS5611 MS5611(0x77); // MS5611 class 생성
 
 void setup() {
-  Serial.begin(115200);
-  Wire.setClock(400000);
-  if (MS5611.begin() == true) {
-    Serial.print("MS5611 begin");
-  }
-  MS5611.setOversampling(OSR_ULTRA_HIGH);  // 모드 설정
+    Serial.begin(115200);
+    Wire.setClock(400000);
+    if (MS5611.begin() == true) {
+        Serial.print("MS5611 begin");
+    }
+    MS5611.setOversampling(OSR_ULTRA_HIGH); // 모드 설정
 }
 
 void loop() {
-  MS5611.read();  // 센서값 read
-  pressure = MS5611.getPressure();
-  temperature = MS5611.getTemperature();
-  altitude = getAltitude(pressure, temperature);
+    MS5611.read(); // 센서값 read
+    pressure = MS5611.getPressure();
+    temperature = MS5611.getTemperature();
+    altitude = getAltitude(pressure, temperature);
 
     Serial.print("Temperture : ");
     Serial.print(temperature);
     Serial.print("  Pressure : ");
     Serial.print(pressure);
     Serial.print("  Altitude : ");
-//   Serial.println(altitude);
-  delay(100);
+    Serial.println(altitude);
+    delay(100);
 }
 
 float getAltitude(float pressure, float temperature) {
-  return (1.0f - powf((pressure / sea_pressure), 0.190284f)) * (temperature + 273.15f) / 0.0065f;
-  //   return (1.0f - powf((pressure / sea_pressure), 0.190284f)) * 44307.694f;
+    return (1.0f - powf((pressure / sea_pressure), 0.190284f)) * (temperature + 273.15f) / 0.0065f;
+    //   return (1.0f - powf((pressure / sea_pressure), 0.190284f)) * 44307.694f;
 }
